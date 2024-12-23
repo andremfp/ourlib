5b6ccd29bc27a258453f5ebd22e03dfcf093491f
andremfp <andremfp29@gmail.com>
2024-12-23 17:19:33 +0000

    remove explicit css stylings

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,8 @@
 <template>
-  <div id="app">
-    <main>
+  <div id="app" class="h-full flex flex-col">
+    <main class="flex-1 flex justify-center items-center">
       <!-- The router-view renders the matched route's component -->
       <router-view />
     </main>
   </div>
 </template>
-
-<script setup>
-import { onMounted } from "vue";
-
-onMounted(() => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.log("Service Worker registration failed:", error);
-      });
-  } else {
-    console.log("Service Worker not supported in this browser.");
-  }
-});
-</script>
