8450f39f2e0bf0dc27d8782da8d80d6f03ebf1c7
andremfp <andremfp29@gmail.com>
2025-01-09 18:51:57 +0000

    fix vercel proxy - wip

--- api/goodreads-proxy.ts
+++ api/goodreads-proxy.ts
@@ -23,7 +23,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       throw new Error("No stripped URL found");
     }
 
-    const match = stripped.match(\/book\/isbn\/\d+/);
+    const match = stripped.match(/book\/isbn\/\d+/);
 
     let originalPath = "";
     if (match) {
