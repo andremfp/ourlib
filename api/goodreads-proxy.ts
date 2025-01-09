b6fdcfe0a7504fe2ec00400b87d2a8951522b5be
andremfp <andremfp29@gmail.com>
2025-01-09 18:24:41 +0000

    fix vercel proxy - wip

--- api/goodreads-proxy.ts
+++ api/goodreads-proxy.ts
@@ -10,7 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   console.log("=== API HANDLER STARTED ===");
   console.log("Method:", req.method);
   console.log("URL:", req.url);
-  console.log("Query:", JSON.stringify(req.query));
+  console.log("Query:", req.query);
 
   // Add error boundary
   try {
