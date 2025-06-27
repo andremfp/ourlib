13ecc0ab393da62ed631a5b3e755e3ec7caf57f2
andremfp <andremfp29@gmail.com>
2025-06-27 15:04:29 +0100

    wip

--- src/components/modals/AddLibrary.vue
+++ src/components/modals/AddLibrary.vue
@@ -4,6 +4,10 @@ import { getAuth } from "firebase/auth";
 import { createLibrary } from "@/apis/libraryAPI";
 import type { Library } from "@/schema";
 import { UI_LIMITS } from "@/constants/constants";
+import { firestore } from "@/firebase";
+import { doc, DocumentReference } from "firebase/firestore";
+import { COLLECTION_NAMES } from "@/constants";
+import type { User } from "@/schema";
 
 // ============= PROPS & EMITS =============
 const props = defineProps<{
@@ -88,10 +92,16 @@ async function handleSubmit() {
     isSubmitting.value = true;
     errorMessage.value = "";
 
+    const userRef = doc(
+      firestore,
+      COLLECTION_NAMES.USERS,
+      userId,
+    ) as DocumentReference<User>;
+
     // Create the library
     const newLibrary = await createLibrary({
       name: trimmedName,
-      ownerId: userId,
+      owner: userRef,
       booksCount: 0,
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString(),
